3c0c3d02eadb2b835201b84e3bbb28e04b930c16
ulises-jeremias <ulisescf.24@gmail.com>
2022-09-06 14:50:54 -0300

    Updated handler

--- examples/serverless/serverless-s3-local/src/handlers/s3/on-object-created.py
+++ examples/serverless/serverless-s3-local/src/handlers/s3/on-object-created.py
@@ -2,6 +2,9 @@ import os
 import boto3
 
 
-def handler(_event, _context):
-    print("Hello World")
-
+def handler(event, _context):
+    print("on-object-created handler called")
+    filename = event["Records"][0]["s3"]["object"]["key"]
+    bucketname = event["Records"][0]["s3"]["bucket"]["name"]
+    print(f"File uploaded to S3 bucket: {bucketname}")
+    print(f"Filename: {filename}")
