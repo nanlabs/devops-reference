06f34f53760dfd20bbcac2b33e5bf0e8f562e41d
ulises-jeremias <ulisescf.24@gmail.com>
2023-02-24 13:58:59 -0300

    Updated examples

--- examples/terraform/terraform-aws-minecraft-server/examples/existing-s3-bucket/main.tf
+++ examples/terraform/terraform-aws-minecraft-server/examples/existing-s3-bucket/main.tf
@@ -5,21 +5,5 @@ provider "aws" {
 module "minecraft" {
   source = "../../"
 
-  name        = "minecraft"
-  namespace   = "games"
-  environment = "games"
-
-  vpc_id    = "vpc-xxxxxxxxxxxxxxxx"
-  subnet_id = "subnet-xxxxxxxxxxxxxxxx"
-
-  minecraft_port        = 25565
-  minecraft_root        = "/home/minecraft"
-  minecraft_version     = "1.19.3"
-  minecraft_backup_freq = 10
-
-  java_ms_mem = "2G"
-  java_mx_mem = "2G"
-
-
-  tags = { By = "me" }
+  bucket_name = "games-minecraft-abcdef123456"
 }
