1fac3d422b5ad2c38b839f7799993078bbbd15d1
ulises-jeremias <ulisescf.24@gmail.com>
2023-03-06 18:07:43 -0300

    Added example

---
+++ examples/serverless-start-stop-ec2-instance/src/handlers/start.ts
@@ -0,0 +1,29 @@
+import { ScheduledHandler } from "aws-lambda";
+import { EC2 } from "aws-sdk";
+
+export const handler: ScheduledHandler = async (event) => {
+  const ec2 = new EC2({ region: event.region });
+
+  const instanceId = process.env.EC2_INSTANCE_ID;
+
+  if (!instanceId) {
+    throw new Error("EC2_INSTANCE_ID is not defined");
+  }
+
+  // check if instance is running. If not, start it
+  const instanceStatus = await ec2.describeInstanceStatus({
+    InstanceIds: [instanceId],
+  }).promise();
+
+  if (instanceStatus?.InstanceStatuses && instanceStatus?.InstanceStatuses[0].InstanceState?.Name === "running") {
+    console.log("Instance is already running");
+    return;
+  }
+
+  const result = await ec2.startInstances({ InstanceIds: [instanceId] }).promise();
+  if (result.$response.error) {
+    throw result.$response.error;
+  }
+
+  console.log(result);
+};
---
+++ examples/serverless-start-stop-ec2-instance/src/handlers/stop.ts
@@ -0,0 +1,29 @@
+import { ScheduledHandler } from "aws-lambda";
+import { EC2 } from "aws-sdk";
+
+export const handler: ScheduledHandler = async (event) => {
+  const ec2 = new EC2({ region: event.region });
+
+  const instanceId = process.env.EC2_INSTANCE_ID;
+
+  if (!instanceId) {
+    throw new Error("EC2_INSTANCE_ID is not defined");
+  }
+
+  // check if instance is running. If yes, stop it
+  const instanceStatus = await ec2.describeInstanceStatus({
+    InstanceIds: [instanceId],
+  }).promise();
+
+  if (instanceStatus?.InstanceStatuses && instanceStatus?.InstanceStatuses[0].InstanceState?.Name !== "running") {
+    console.log("Instance not running. Skipping");
+    return;
+  }
+
+  const result = await ec2.stopInstances({ InstanceIds: [instanceId] }).promise();
+  if (result.$response.error) {
+    throw result.$response.error;
+  }
+
+  console.log(result);
+};
