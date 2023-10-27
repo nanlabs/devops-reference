5a9c8ea14e5bf38116bba138b30fc8285872f50b
Ulises Jeremias <ulisescf.24@gmail.com>
2023-10-26 23:09:03 -0300

    Update mongo-init.sh

--- examples/compose-mongodb/initdb.d/mongo-init.sh
+++ examples/compose-mongodb/initdb.d/mongo-init.sh
@@ -7,14 +7,15 @@ set -e
 MONGO_NON_ROOT_ROLE="${MONGO_NON_ROOT_ROLE:-readWrite}"
 
 if [ -n "${MONGO_NON_ROOT_USERNAME:-}" ] && [ -n "${MONGO_NON_ROOT_PASSWORD:-}" ]; then
+  # shellcheck disable=SC2154
   "${mongo[@]}" "$MONGO_INITDB_DATABASE" <<-EOJS
-		db.createUser({
-			user: $(_js_escape "$MONGO_NON_ROOT_USERNAME"),
-			pwd: $(_js_escape "$MONGO_NON_ROOT_PASSWORD"),
-			roles: [ { role: $(_js_escape "$MONGO_NON_ROOT_ROLE"), db: $(_js_escape "$MONGO_INITDB_DATABASE") } ]
-			})
+  db.createUser({
+    user: $(_js_escape "$MONGO_NON_ROOT_USERNAME"),
+    pwd: $(_js_escape "$MONGO_NON_ROOT_PASSWORD"),
+    roles: [ { role: $(_js_escape "$MONGO_NON_ROOT_ROLE"), db: $(_js_escape "$MONGO_INITDB_DATABASE") } ]
+  })
 	EOJS
 else
-    # print warning or kill temporary mongo and exit non-zero
-    exit 1
+  # print warning or kill temporary mongo and exit non-zero
+  exit 1
 fi
